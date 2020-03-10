b13ae1010ac911da68e7b3a464bdc637ed637852
dmahno <dmitrymahno@mail.ru>
2020-03-10 16:45:22 +0300

    changed modifier seperation from - to _

--- src/pages/main/main.page.jsx
+++ src/pages/main/main.page.jsx
@@ -4,7 +4,7 @@ import image from '../../assets/icons/icon-main-page.svg';
 
 const Main = () => {
   return (
-    <div className='content flex-center'>
+    <div className='content flex_center'>
       <InfoBlock
         image={image}
         text='Configure repository connection and synchronization settings'
